0875259379a6736a99f23bde5fd998478fcc9df8
Alessiobettens <alessio.bettens@student.ehb.be>
2026-05-30 23:12:45 +0200

    add product page without price and image

--- App.js
+++ App.js
@@ -108,20 +108,80 @@ function DetailScreen({ route }) {
 }
 
 function ProductenScreen() {
+  const [products, setProducts] = useState([]);
+  const [skus, setSkus] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://api.webflow.com/v2/collections/6a04927dcf62d2e49fcf7e28/items",
+      {
+        headers: {
+          Authorization:
+            "Bearer c438614a6dd5bcd906bb481dd394634d08798782a180ff00c2a15243ac0da0b2",
+        },
+      },
+    )
+      .then((res) => res.json())
+      .then((data) => setProducts(data.items || []));
+
+    fetch(
+      "https://api.webflow.com/v2/sites/69fdf72b307dcc6cf820875b/products/skus",
+      {
+        headers: {
+          Authorization:
+            "Bearer c438614a6dd5bcd906bb481dd394634d08798782a180ff00c2a15243ac0da0b2",
+        },
+      },
+    )
+      .then((res) => res.json())
+      .then((data) => setSkus(data.items || []));
+  }, []);
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Producten scherm</Text>
+    <View
+      style={{ flex: 1, paddingHorizontal: 15, backgroundColor: "#f2f2f2" }}
+    >
+      <FlatList
+        data={products}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => {
+          const sku = skus.find((s) => s.id === item.fieldData["default-sku"]);
+
+          return (
+            <View
+              style={{
+                marginBottom: 20,
+                backgroundColor: "#fff",
+                borderRadius: 12,
+                padding: 12,
+              }}
+            >
+              <Image
+                source={{
+                  uri:
+                    sku?.fieldData?.image?.url ||
+                    "https://via.placeholder.com/300",
+                }}
+                style={{ width: "100%", height: 150, borderRadius: 10 }}
+              />
+
+              <Text style={{ fontSize: 18, fontWeight: "bold", marginTop: 10 }}>
+                {item.fieldData.name}
+              </Text>
+
+              <Text style={{ color: "#555", marginTop: 5 }}>
+                € {sku?.price?.value ? sku.price.value / 100 : "Geen prijs"}
+              </Text>
+            </View>
+          );
+          console.log(sku);
+        }}
+      />
     </View>
   );
 }
 
-function CampussenScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Campussen scherm</Text>
-    </View>
-  );
-}
+
 
 function HomeStack() {
   return (
@@ -138,7 +198,6 @@ export default function App() {
       <Tab.Navigator>
         <Tab.Screen name="Nieuws" component={HomeStack} />
         <Tab.Screen name="Producten" component={ProductenScreen} />
-        <Tab.Screen name="Campussen" component={CampussenScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
