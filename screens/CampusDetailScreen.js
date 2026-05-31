d7984c884788bffe443ce985e18a49c189d087fa
Alessiobettens <alessio.bettens@student.ehb.be>
2026-05-31 23:57:42 +0200

    add focus + scrollview to CampusDetailScreen

--- screens/CampusDetailScreen.js
+++ screens/CampusDetailScreen.js
@@ -1,15 +1,41 @@
 import React, { useEffect, useState } from "react";
-import { View, FlatList, TouchableOpacity, Image, Text } from "react-native";
+import {
+  View,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  Text,
+  ScrollView,
+} from "react-native";
 
 export default function CampusDetailScreen({ route }) {
   const { item } = route.params;
+  const [focusList, setFocusList] = useState([]);
 
+  const focusName = focusList.find((f) => f.id === item.fieldData.focus)
+    ?.fieldData?.name;
+
+  useEffect(() => {
+    fetch(
+      "https://api.webflow.com/v2/collections/6a14490591ad6eaec8519231/items",
+      {
+        headers: {
+          Authorization:
+            "Bearer c438614a6dd5bcd906bb481dd394634d08798782a180ff00c2a15243ac0da0b2",
+        },
+      },
+    )
+      .then((res) => res.json())
+      .then((data) => setFocusList(data.items || []));
+  }, []);
   return (
-    <View
+    <ScrollView
       style={{
         flex: 1,
-        padding: 20,
         backgroundColor: item.fieldData.kleur || "#fff",
+      }}
+      contentContainerStyle={{
+        padding: 20,
       }}
     >
       <Image
@@ -48,17 +74,28 @@ export default function CampusDetailScreen({ route }) {
         <Text style={{ color: "#333" }}>{item.fieldData.adres}</Text>
       </View>
 
-      <Text style={{ marginTop: 10, color: "#fff" }}>
-        Focus: {item.fieldData.focus}
-      </Text>
+      <View
+        style={{
+          marginTop: 10,
+          backgroundColor: "#fff",
+          paddingHorizontal: 12,
+          paddingVertical: 5,
+          borderRadius: 5,
+          alignSelf: "flex-start", // ✅ SUPER BELANGRIJK
+        }}
+      >
+        <Text style={{ color: "#333", fontWeight: "bold" }}>
+          {focusName ?? "Geen focus"}
+        </Text>
+      </View>
 
-      <Text style={{ marginTop: 10, lineHeight: 20, color: "#fff" }}>
+      <Text style={{ marginTop: 10, lineHeight: 20, color: "#ffffff" }}>
         {item.fieldData["beschrijving-3"]?.replace(/<[^>]*>/g, "")}
       </Text>
 
-      <Text style={{ marginTop: 15, fontStyle: "italic", color: "#555" }}>
+      <Text style={{ marginTop: 10, fontStyle: "italic", color: "#555" }}>
         {item.fieldData["contact-info"]?.replace(/<[^>]*>/g, "")}
       </Text>
-    </View>
+    </ScrollView>
   );
 }
