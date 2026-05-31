3381cd4c2936621780e519ca9a53f48ae172a741
Alessiobettens <alessio.bettens@student.ehb.be>
2026-05-31 23:03:42 +0200

    Added styles  to campus detail screen

--- screens/CampusDetailScreen.js
+++ screens/CampusDetailScreen.js
@@ -5,7 +5,13 @@ export default function CampusDetailScreen({ route }) {
   const { item } = route.params;
 
   return (
-    <View style={{ flex: 1, padding: 20 }}>
+    <View
+      style={{
+        flex: 1,
+        padding: 20,
+        backgroundColor: item.fieldData.kleur || "#fff",
+      }}
+    >
       <Image
         source={{
           uri:
@@ -14,21 +20,45 @@ export default function CampusDetailScreen({ route }) {
         style={{ width: "100%", height: 200, borderRadius: 10 }}
       />
 
-      <Text style={{ fontSize: 22, fontWeight: "bold", marginTop: 10 }}>
+      <Text
+        style={{
+          fontSize: 25,
+          fontWeight: "bold",
+          marginTop: 10,
+          color: "#ffffff",
+        }}
+      >
         {item.fieldData.name}
       </Text>
 
-      <Text style={{ marginTop: 5 }}>{item.fieldData.adres}</Text>
+      <View
+        style={{
+          backgroundColor: "#ffffff",
+          marginTop: 10,
+          paddingVertical: 8,
+          paddingHorizontal: 30,
+          borderRadius: 5,
+          flexDirection: "row",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <Text style={{ marginRight: 20 }}>📍</Text>
 
-      <Text style={{ marginTop: 10 }}>Focus: {item.fieldData.focus}</Text>
+        <Text style={{ color: "#333" }}>{item.fieldData.adres}</Text>
+      </View>
 
-      <Text style={{ marginTop: 10, lineHeight: 20 }}>
+      <Text style={{ marginTop: 10, color: "#fff" }}>
+        Focus: {item.fieldData.focus}
+      </Text>
+
+      <Text style={{ marginTop: 10, lineHeight: 20, color: "#fff" }}>
         {item.fieldData["beschrijving-3"]?.replace(/<[^>]*>/g, "")}
       </Text>
 
-        <Text style={{ marginTop: 15, fontStyle: "italic", color: "#555" }}>
-          {item.fieldData["contact-info"]?.replace(/<[^>]*>/g, "")}
-        </Text>
+      <Text style={{ marginTop: 15, fontStyle: "italic", color: "#555" }}>
+        {item.fieldData["contact-info"]?.replace(/<[^>]*>/g, "")}
+      </Text>
     </View>
   );
 }
