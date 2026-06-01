96f21a154a5f0e8b65a692e64fff910877930956
Alessiobettens <alessio.bettens@student.ehb.be>
2026-06-01 23:06:47 +0200

    add products

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -2,32 +2,22 @@ import React from "react";
 import { View, Text, Image } from "react-native";
 import { styles } from "../styles/styles";
 
-export default function ProductCard({ item, skus }) {
-
-  const sku = skus.find(
-    (s) => s.id === item.fieldData["default-sku"]
-  );
+export default function ProductCard({ item }) {
+  const image = item.fieldData.afbeelding?.url;
+  const price = item.fieldData.prijs;
 
   return (
     <View style={styles.card}>
-      
       <Image
         source={{
-          uri:
-            sku?.fieldData?.image?.url ||
-            "https://via.placeholder.com/300",
+          uri: image || "https://via.placeholder.com/300",
         }}
         style={styles.image}
       />
 
-      <Text style={styles.title}>
-        {item.fieldData.name}
-      </Text>
+      <Text style={styles.title}>{item.fieldData.name}</Text>
 
-      <Text style={styles.intro}>
-        € {sku?.price?.value ? sku.price.value / 100 : "Geen prijs"}
-      </Text>
-
+      <Text style={styles.intro}>€ {price ?? "Geen prijs"}</Text>
     </View>
   );
 }
--- screens/ProductenScreen.js
+++ screens/ProductenScreen.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { View, FlatList, TouchableOpacity, Image, Text } from "react-native";
+import { View, FlatList } from "react-native";
 import ProductCard from "../components/ProductCard";
 
 export default function ProductenScreen() {
   const [products, setProducts] = useState([]);
-  const [skus, setSkus] = useState([]);
+  console.log(products);
 
   useEffect(() => {
     fetch(
@@ -17,29 +17,23 @@ export default function ProductenScreen() {
       },
     )
       .then((res) => res.json())
-      .then((data) => setProducts(data.items || []));
-
-    fetch(
-      "https://api.webflow.com/v2/sites/69fdf72b307dcc6cf820875b/products/skus",
-      {
-        headers: {
-          Authorization:
-            "Bearer c438614a6dd5bcd906bb481dd394634d08798782a180ff00c2a15243ac0da0b2",
-        },
-      },
-    )
-      .then((res) => res.json())
-      .then((data) => setSkus(data.items || []));
+      .then((data) => {
+        setProducts(data.items || []);
+      });
   }, []);
 
   return (
     <View
-      style={{ flex: 1, paddingHorizontal: 15, backgroundColor: "#f2f2f2" }}
+      style={{
+        flex: 1,
+        paddingHorizontal: 15,
+        backgroundColor: "#f2f2f2",
+      }}
     >
       <FlatList
         data={products}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <ProductCard item={item} skus={skus} />}
+        renderItem={({ item }) => <ProductCard item={item} />}
       />
     </View>
   );
